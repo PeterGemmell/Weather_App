8447501c049196672e1bb5f39b0eb81e3fe248aa
Peter Gemmell <petergemmell65@hotmail.com>
2020-05-12 17:22:45 +0100

    api call working and updating

---
+++ weather/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=518e476d95d91f071794a98ed064286e'
+    city = 'Las Vegas'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+
+    context = {'weather' : weather}
+    return render(request, 'weather/index.html', context) #returns the index.html template
+
+# Create your views here.
